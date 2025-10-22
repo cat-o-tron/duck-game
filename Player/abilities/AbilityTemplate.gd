08ca6dace63e1bdab546053d73907a83fcfe3621
cat-o-tron <ekainmartin@upaikaslea.org>
2025-10-22 21:41:32 +0200

    bomb added
    added the bomb ability and created the  player damage but I'm yet to implement player damage.

--- Player/abilities/AbilityTemplate.gd
+++ Player/abilities/AbilityTemplate.gd
@@ -5,7 +5,10 @@ signal actPress
 signal actHold(timeHeld:float)
 signal actRelease
 
+var Player:CharacterBody2D 
+
 func _ready() -> void:
+	Player = get_parent().get_parent()
 	self.connect("actPress", ActPress)
 	self.connect("actHold", ActHold)
 	self.connect("actRelease", ActRelease)
